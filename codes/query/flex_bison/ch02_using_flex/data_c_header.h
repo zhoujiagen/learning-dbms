f6ae3109142b7f873241f378a28b5a540bfb950d
zhoujiagen <zhoujiagen@gmail.com>
2021-11-11 08:26:07 +0800

    [Query] remove code of query parse.

--- codes/query/flex_bison/ch02_using_flex/data_c_header.h
+++
@@ -1,7 +0,0 @@
-/* data header file. */
-int add(int a, int b);
-
-int sub(int a, int b)
-{
-    return a - b;
-}
--- codes/query/flex_bison/ch04_parsing_sql/sql/1.sql
+++
@@ -1,5 +0,0 @@
--- SELECT name, flavor
--- FROM Foods
--- WHERE Foods.type = "fruit";
-
-select a,b,c from d;
